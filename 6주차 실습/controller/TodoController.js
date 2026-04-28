fd9e8f9aaa4f619d6f7f8a9d851857a50f8b6bf2
gihoon1237 <gihoon1237@gmail.com>
2026-04-28 20:20:55 +0900

    6주차 과제 완료

---
+++ 6주차 실습/controller/TodoController.js
@@ -0,0 +1,43 @@
+import Todo from "../DOM/Todo.js";
+import CompleteController from "./CompleteController.js";
+
+class TodoController {
+    constructor(todo) {
+        this.newTodo=new Todo(todo);
+        this.delBtnNode=this.newTodo.getDelBtn();
+        this.comBtnNode=this.newTodo.getCompleteBtn();
+        this.innerNode=this.newTodo.getInnerText();
+
+        this.delBtnNode.addEventListener('click', () => {
+            this.delTodo();
+        });
+
+        this.comBtnNode.addEventListener('click', () => {
+            this.doneTodo();
+        });
+    }
+        addTodo(){
+            const todoList=document.getElementById('to-do-list');
+            const input=document.querySelector('input');
+            todoList.appendChild(this.newTodo.addRow());
+            input.value='';
+        }
+    
+        delTodo(){
+            //<div id="to-do-list"></div>
+            const todoList=document.getElementById('to-do-list');
+            todoList.removeChild(this.newTodo.getRow());
+        }
+        doneTodo(){
+
+            const completeController =
+            new CompleteController(this.innerNode.innerText);
+
+            completeController.addComplete();
+
+            this.newTodo.getRow().remove();
+        }
+        
+}
+
+export default TodoController;
